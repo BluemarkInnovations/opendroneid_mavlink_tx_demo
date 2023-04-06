aa69426d1bec6ec3902d13ed2cea8a1673ca3a18
BlueMark Innovations BV <roel@bluemark.io>
2023-04-06 13:20:23 +0200

    create bin folder, fixed

--- build.opendroneid.sh
+++ build.opendroneid.sh
@@ -2,11 +2,12 @@
 # (c) Bluemark Innovations BV 
 # MIT license
 
+mkdir bin
 cd opendroneid-core-c
 rm -R build
 #git submodule update --init
 mkdir build && cd build
-mkdir bin
+
 cmake ../.
 make
 
